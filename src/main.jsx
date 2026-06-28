e9714ba82e40d5d3a50ceae1949ef2031eabac2e
ASM-WOKSTATION\Hp <shibly.dhk@gmail.com>
2026-06-28 19:44:06 +0600

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { initTheme } from './theme.js'
@@ -7,7 +8,7 @@ import './index.css'
 
 initTheme()
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <App />
